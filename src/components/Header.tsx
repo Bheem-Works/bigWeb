6341a27a185254f02ac1ef058993625f254440a4
bheem-works <shokokomichan098@gmail.com>
2025-08-10 17:29:17 +0545

    push the new header sections

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { type FunctionComponent } from "react";
+
+export const Header: FunctionComponent =()=> {
+return (
+    <div>
+        {/* here will be the header section the web section template;  */}
+        
+    </div>
+);
+};
